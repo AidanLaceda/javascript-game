74c03d19c9be8fcf04ea8565fcc80520bcd6bcf1
Aidan Laceda <mralaceda@gmail.com>
2025-09-16 23:46:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request, url_for
-from database import get_db, close_db
-
 
 app = Flask(__name__)
-app.teardown_appcontext(close_db)
 
 @app.route("/")
 def index():
@@ -12,18 +9,3 @@ def index():
 @app.route("/game")
 def game():
     return render_template("game.html")
-
-@app.route("/leaderboard")
-def leaderboard():
-    db = get_db()
-    leaderboard = db.execute("""SELECT * FROM leaderboard""").fetchall()
-    return render_template("leaderboard.html", title="Leaderboard", leaderboard=leaderboard)
-
-@app.route("/store_score", methods=["POST"])
-def store_score():
-    score = int(request.form["score"])
-    db = get_db()
-    db.execute("""INSERT INTO leaderboard (score)
-                VALUES (?);""", (score,))
-    db.commit()
-    return "success"
